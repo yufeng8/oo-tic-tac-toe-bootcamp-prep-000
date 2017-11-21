ecf26fd5fef6093fe18450f632dd1e1f3c30cd93
Yufeng Liu Reach <yufeng8@vt.edu>
2017-11-21 18:18:08 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -27,7 +27,7 @@ class TicTacToe
   end
 
   def valid_move?(board, index)
-    index.between?(0,8) && !position_taken?(@board, index)
+    index.between?(0,8) && !position_taken?(board, index)
   end
 
   def turn
